5ea5c36e08fb2cb9f9fb4691b03cf7152e9c5ddf
陈磊 <gbdvjxu1257q@outlook.com>
2023-04-19 21:57:34 +0800

    add public ps

---
+++ packs/Npm-PackAndPush-All.ps1
@@ -0,0 +1,13 @@
+﻿# 版本号更新/打包
+$version = Read-Host '请输入版本号！'
+Get-ChildItem .\ | ?{$_.psiscontainer -eq $true} | foreach-object -process{
+    $directory = $_.Name;
+    cd $directory
+    npm version $version
+    npm publish --access public
+    cd ../
+}
+
+"`n按任意键退出：" ;
+[Console]::Readkey() |　Out-Null ;
+Exit ;
