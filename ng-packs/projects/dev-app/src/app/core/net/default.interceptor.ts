07b9a71389d5e0efc9b442b1b6c945aaaedb06f6
snowchenlei <gbdvjxu1257q@outlook.com>
2024-11-25 17:41:51 +0800

    update ng@18

--- ng-packs/projects/dev-app/src/app/core/net/default.interceptor.ts
+++ ng-packs/projects/dev-app/src/app/core/net/default.interceptor.ts
@@ -1,10 +1,4 @@
-import {
-  HttpErrorResponse,
-  HttpHandlerFn,
-  HttpInterceptorFn,
-  HttpRequest,
-  HttpResponseBase,
-} from '@angular/common/http';
+import { HttpErrorResponse, HttpHandlerFn, HttpInterceptorFn, HttpRequest, HttpResponseBase } from '@angular/common/http';
 import { Injector, inject } from '@angular/core';
 import { IGNORE_BASE_URL, _HttpClient } from '@delon/theme';
 import { environment } from '@env/environment';
